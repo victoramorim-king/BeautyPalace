f07f70cf1f8a42e1bab90585729dce48f0ad76ec
Victor Amorim <victoramorimchagas@gmail.com>
2023-02-10 15:56:20 -0300

    get the tomorrow agenda data

--- index.js
+++ index.js
@@ -6,41 +6,24 @@ const querystring = require('querystring');
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-
-  await page.setRequestInterception(true);
-
-    let dir = 0;
-  page.on('request', async request => {
-    if (request.url() === 'https://admin.salaovip.com.br/admin/agenda/carregarAgenda' && dir == 0) {
-      dir = 1
-      const postData = request.postData().replace('2023-02-07', '2023-02-09')
-
-      console.log(postData)
-     var data = {
-        'method': 'POST',
-        'postData': querystring.stringify(postData),
-        'headers': {
-            ...request.headers(),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-    };
-    request.continue(data);  
-    }
-    else {
-      request.continue();
-    }
-  });
   await page.goto('https://admin.salaovip.com.br/beautypalace/admin');
   await page.type('#formEmail', config.email)
   await page.type('#formSenha', config.passWord)
   await page.click('.btn-login')
+  await page.waitForResponse('https://admin.salaovip.com.br/admin/agenda/carregarAgenda')
+  await page.click('xpath//html/body/div[10]/div[2]/div[4]/div[2]/div[1]/div[2]/div[1]/ul/li[5]')
+  await page.waitForResponse('https://admin.salaovip.com.br/admin/agenda/carregarAgenda')
+  let tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow = String(tomorrow.getDate())
+  await page.click(`xpath///html/body/div[10]/div[2]/div[3]/div[2]/div[1]/div/div[1]/table/tbody/*/td[contains(text(), "${tomorrow}") and not(contains(@class, "old"))]`)
   const agendamentos = await page.waitForResponse('https://admin.salaovip.com.br/admin/agenda/carregarAgenda')
-        const response = await page.goto("https://admin.salaovip.com.br/admin/agenda/carregarAgenda");
+  console.log(await agendamentos.json())
+  let data = JSON.stringify(await agendamentos.json(), null, 2);
+  fs.writeFile('agendamentos.json', data, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
 
-      console.log({
-        url: response.url(),
-        statusCode: response.status(),
-        body: await response.text()
-      });
   await browser.close();
 })();
