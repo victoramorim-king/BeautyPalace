63f24a503851634f6bc7e686b81e309f6b46ff75
Victor Amorim <victoramorimchagas@gmail.com>
2023-02-17 16:44:17 -0300

    Using new build pack

--- sendRemindersViaWhatsapp.js
+++ sendRemindersViaWhatsapp.js
@@ -69,7 +69,7 @@ Posso confirmar o seu horário?
     chatId = "55" + '11981451586' + "@c.us";
     client.sendMessage(chatId, "Esses agendamentos não puderam ser enviados por conter algum erro em seus dados: \n" + JSON.stringify(remindersWithErrors)).finally()
 
-    console.log("aguardando 2 minutos para garantir que todas as mensagens foram enviadas!")
+    console.log("Aguardando 2 minutos para garantir que todas as mensagens foram enviadas!")
     setTimeout(function() {
       client.destroy()
     }, 120000);
