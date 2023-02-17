8a3678017700be34979f28035eca01f6bc9ef956
Victor Amorim <victoramorimchagas@gmail.com>
2023-02-17 16:30:19 -0300

    configure puppeteer to run on heroku

--- sendRemindersViaWhatsapp.js
+++ sendRemindersViaWhatsapp.js
@@ -1,6 +1,9 @@
 const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox', "--disable-setuid-sandbox"]
+  },
   authStrategy: new LocalAuth()
 });
 
